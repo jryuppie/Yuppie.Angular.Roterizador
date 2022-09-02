4f25e0e74bc366a32d9f25cee0fcfe6782dd4d9b
jonathanrossato <jonathan.e.rossato@gmail.com>
2022-09-02 12:39:43 -0300

    branch Yuppie

--- src/app/menu-lateral/menu-lateral.component.ts
+++ src/app/menu-lateral/menu-lateral.component.ts
@@ -17,25 +17,15 @@ export class MenuLateralComponent implements OnInit {
 
   valorTesteInput?: string
 
-  temaLuksColor: boolean = false;
+  
 
   constructor(private menuLateralService: MenuLateralService,private router: Router) { }
 
   ngOnInit(): void {
 
 
-    (document.getElementById('sidebar') as HTMLElement).style.setProperty('background-image', 'url(https://www.autoluks.com.br/uploads/1/1/5/6/115643989/background-images/1817667807.jpg)')
-    this.temaLuksColor = false;
-    (document.getElementById("mudarTema1") as HTMLElement).addEventListener(
-      "click", () => {
-        this.mudarTema()
-      }
-    );
-    (document.getElementById("mudarTema2") as HTMLElement).addEventListener(
-      "click", () => {
-        this.mudarTema()
-      }
-    );
+    // (document.getElementById('sidebar') as HTMLElement).style.setProperty('background-image', 'url(https://www.autoluks.com.br/uploads/1/1/5/6/115643989/background-images/1817667807.jpg)')   
+    
 
     this.menuLateralService?.setTitulo('lateral')!
     
@@ -54,39 +44,7 @@ export class MenuLateralComponent implements OnInit {
     this.titulo = titulo;
   }
 
-  items!: MenuItem[];
-  mudarTema() {
-    this.items = [
-      { label: 'Usuários', icon: 'pi pi-fw pi-home', routerLink: ["/configuracoes"] }
-    ];
-
-
-
-
-    let cor1 = '';
-    let cor2 = '';
-    let bgSidebar = '';
-    if (this.temaLuksColor) {
-      cor1 = getComputedStyle(document.documentElement)
-        .getPropertyValue('--cor-base-autoluks');
-      cor2 = getComputedStyle(document.documentElement)
-        .getPropertyValue('--cor-base-complementar-autoluks');
-      (document.getElementById('sidebar') as HTMLElement).style.setProperty('background-image', 'url(https://www.autoluks.com.br/uploads/1/1/5/6/115643989/background-images/1817667807.jpg)');
-      this.temaLuksColor = false;
-    }
-    else {
-      (document.getElementById('sidebar') as HTMLElement).style.setProperty('background-image', 'url()');
-      (document.getElementById('sidebar') as HTMLElement).style.setProperty('background-image', cor1);
-      cor1 = getComputedStyle(document.documentElement)
-        .getPropertyValue('--cor-base-lukscolor');
-      cor2 = getComputedStyle(document.documentElement)
-        .getPropertyValue('--cor-base-complementar-lukscolor');
-      this.temaLuksColor = true;
-    }
-    (document.querySelector(':root') as HTMLElement).style.setProperty('--cor-base', cor1);
-    (document.querySelector(':root') as HTMLElement).style.setProperty('--cor-base-complementar', cor2);
-
-  }
+  items!: MenuItem[]; 
 
 }
 
